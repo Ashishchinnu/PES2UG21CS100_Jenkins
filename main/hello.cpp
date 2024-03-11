ad8b3aacca3ed43f6f3d997ba1fc859e0e0e2255
Ashishchinnu <ashishreddychinnu81@gmail.com>
2024-03-11 12:09:11 +0530

    main

--- main/hello.cpp
+++ main/hello.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main() {
   cout << "Hello, World,This is PES2UG21CS100\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Webhook successfully enabled\n";
+  cout << "Task 3 done\n";
   return 0;
 }
